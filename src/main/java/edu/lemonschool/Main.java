a3b36d7c479ec27d3858fdab22028574bdfc9392
Yaroslav <jemesjohan@gmail.com>
2023-09-20 22:23:01 +0300

    console commit

--- src/main/java/edu/lemonschool/Main.java
+++ src/main/java/edu/lemonschool/Main.java
@@ -2,6 +2,10 @@ package edu.lemonschool;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Hello world!");
+        }
+
     }
 }
